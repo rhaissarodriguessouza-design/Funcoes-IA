748220d1451a0e37de72feca49fa8f416595524e
rhaissarodriguessouza-design <rhaissa.rodrigues.souza@escola.pr.gov.br>
2026-09-10 15:30:45 +0000

    Aula 6 e 7

--- main.js
+++ main.js
@@ -46,10 +46,16 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+  if (atual >= perguntas.length) {
+    mostraResultado();
+    return;
+  }
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  caixaAlternativas.textContent = "";
   mostraAlternativas();
 }
 
@@ -57,7 +63,23 @@ function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.addEventListener("click", () => {
+      respostaSelecionada(alternativa);
+    });
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+  historiaFinal += opcaoSelecionada + " ";
+  atual++;
+  mostraPergunta();
+}
+
 mostraPergunta();
+
+function mostraResultado() {
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+}
